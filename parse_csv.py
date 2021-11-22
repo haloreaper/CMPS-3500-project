a0269b3da85091eb67f85ae73ded7edc6c1d7422
Matthew Taylor <mtaylor41@csub.edu>
2021-11-21 21:33:42 -0800

    Added some form of menu options, can get errors easily if input is bad

--- parse_csv.py
+++ parse_csv.py
@@ -1,10 +1,22 @@
+# Course: CMPS 3500
+# Class Project
+# Python Implementation of a custom statistics summary calculator
+# Date: 11/21/21
+# Student 1: Matthew Taylor
+# Student 2: Daisy Cortez
+# Student 3: Joseph Gaede
+# Description: Data Loading, Cleaning, Searching, and Implementation of statistics summary calculator
+
+
 import csv
 import re
 
+# Iterates through a list of dicts and prints each line
 def printDict(mydict):
     for line in mydict:
         print(line)
 
+# Creates a list of dicts from a csv file
 def csv_to_dict(filename):
     result_list=[]
     with open(filename) as file_obj:
@@ -14,6 +26,7 @@ def csv_to_dict(filename):
             result_list.append(dict(row))
     return result_list
 
+# Takes a value(element), a key(column), and a dict as parameters for searching
 def search(element, column, mydict):
     count = 0
     row = 0
@@ -41,6 +54,9 @@ def search(element, column, mydict):
             print(str(element) + " is present in " + details[i])
 
 
+# Does most of the cleanup related things, removes empty and missing rows first
+# Then removes keys/columns with non-numerical data
+# Finally removes rows with duplicated numbers within the row
 
 def cleanup(mydict):
     row = 0
@@ -73,7 +89,7 @@ def cleanup(mydict):
         values = list(line.values())
         # Find the lines that have duplicated values
         if len(values) != len(set(values)):
-            print("dupes")
+            #print("dupes")
             rows_to_remove.append(line)
     
     # Iterate through array of rows with duplicates and delete them
@@ -83,13 +99,40 @@ def cleanup(mydict):
 
 
 
-            
+# Main functions
 
-# Create a list of dicts
-mydict = csv_to_dict('exampledata.csv')
-print("before")
-printDict(mydict)
-cleanup(mydict)
-print("after")
-printDict(mydict)
-#search('25', "Column A", mydict)
+option = -1
+
+while (option != 0):
+    print("Type one of the options below and hit enter\n")
+    print("Options:")
+    print("1 - Type in a csv file to get data from and clean")
+    print("2 - Type in a value to search for and column to search in")
+    print("3 - Print the previously load csv file")
+    print("0 - Quit")
+
+    option = input("Enter an option...\n")
+
+    if (option == 1):
+        csv_choice = input("Type the file you wish to load 'filename.csv'\n");
+        csv_choice = str(csv_choice)
+        try:
+            mydict = csv_to_dict(csv_choice)
+            cleanup(mydict)
+            print("Data loaded and cleaned\n\n")
+        except:
+            print("Error occured\n")
+        continue
+    if (option == 2):
+        search_choice1 = input("Enter the number you want to search for\n")
+        search_choice1 = str(search_choice1)
+        search_choice2 = input("Enter the column name you want to search in, or enter 'DataSet' to search the entire cleaned csv file\n\n")
+        search_choice2 = str(search_choice2)
+        try:
+            search(search_choice1, search_choice2, mydict)
+        except:
+            print("Error occured\n")
+        continue
+    if (option == 3):
+        printDict(mydict)
+        continue
